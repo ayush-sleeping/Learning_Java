ce86843a7c4d724aadb8e35280712ae583312d28
Ayush Mishra <ayushbm84@gmail.com>
2022-03-26 23:16:26 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+/**
+ *
+ * @author Ayush
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
