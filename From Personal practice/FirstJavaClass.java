59574e7f228d9a8597e1229f56264e1de3523f19
Ayush Mishra <ayushbm84@gmail.com>
2023-06-08 00:32:54 +0530

    Add files via upload

---
+++ From Personal practice/FirstJavaClass.java
@@ -0,0 +1,20 @@
+
+/* 
+ * The name of file and public class should be same, 
+ * Otherwise it will throw compilation error.
+ * IN the file, There can be multiple class, 
+ * But public class is only one .
+
+*/
+public class FirstJavaClass {  
+
+	// From this function, The program will start : 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// If you want to print anything :
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ From Personal practice/variable.java
@@ -0,0 +1,50 @@
+
+public class variable {
+	public static void main(String[] args) {
+		
+		// To store any value we use variables ( In example : Let x = something )
+		// Here "x" is a variable name .
+		
+		/* Variable can be multiple types,
+		  
+		 * String - stores text, such as "Hello". String values are surrounded by double quotes
+		 * int - stores integers (whole numbers), without decimals, such as 123 or -123
+		 * float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+		 * char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+		 * boolean - stores values with two states: true or false
+		 
+		 */
+		
+		/* Syntax : 
+		           type variableName = value;
+		           First :  DataType name
+		           Second : Variable name
+		           Third : Variable's value
+		           
+		   Variable name RULES : 
+		    only you can store _ , $ , AlphaNumeric characters are Alphabets and Digits .
+		    and when you start naming any variable, You can only use Alphabets, _ , $ in starting of variable name.
+		    YOu can also can't use reserved keywords of java in variable name.
+		    
+		 * Examples : */
+		
+		String name = "John";  
+		System.out.println(name);
+		
+		int age = 20; // int 4 byte. and long 2 x 10^18 .
+		System.out.println(age);
+		
+		long bigNumber = 93623692362969l;
+		
+		float averageMarks = 17.867f;
+		
+		char grade = 'A';
+		
+		boolean isValid = false;
+		
+		
+		
+		
+	}
+
+}
