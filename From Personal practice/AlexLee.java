afd910f934a8514f3a9edc6407e9d37c4141678a
Ayush Mishra <ayushbm84@gmail.com>
2023-06-16 00:41:03 +0530

    Add files via upload

---
+++ From Personal practice/AlexLee.java
@@ -0,0 +1,17 @@
+
+public class AlexLee {
+
+	public static void main(String[] args) {
+		// To store data 
+		
+		int a = 8; // This takes value as 5 and store it in "a" .
+		// int is a keyword and it stores integers.
+		System.out.println(a);
+		
+		
+		// To store a name (A bunch of character variable.) we use string .
+		String name = "Ayush B Mishra" ;
+		System.out.println(name.toUpperCase());
+	}
+
+}
