8a54d629b077d6cea81e454949ceb8140e37f0df
Ayush Mishra <ayushbm84@gmail.com>
2023-07-06 20:37:18 +0530

    Create HelloWorld.java

---
+++ Personal Practice/1. Java Syntax & Hello World/HelloWorld.java
@@ -0,0 +1,25 @@
+
+/* 
+ * The name of file and public class should be same, 
+ * Otherwise it will throw compilation error.
+ * IN the file, There can be multiple class, 
+ * But public class is only one .
+
+*/
+public class HelloWorld {  
+
+	// From this function, The program will start : 
+	public static void main(String[] args) {
+		
+		// If you want to print anything :
+		System.out.println("Hello World!");
+
+	}
+
+} 
+
+/* Here, I have learned : 
+ * How to write comments in Java, 
+ * Syntax of Java,
+ * How to print anything.
+ */
