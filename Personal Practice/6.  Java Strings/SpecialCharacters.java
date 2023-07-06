f3edb48592b747f00a3c841f295fb5cf911fc983
Ayush Mishra <ayushbm84@gmail.com>
2023-07-06 22:05:33 +0530

    added

---
+++ Personal Practice/6.  Java Strings/SpecialCharacters.java
@@ -0,0 +1,17 @@
+
+public class SpecialCharacters {
+	
+	/* 
+	 * Special characters : are characters that have special meanings in Java 
+	 * and are represented using escape sequences.
+	 * Common escape sequences include \n for a new line, \t for a tab, \" for a double quote, etc.
+	 */
+	
+	 public static void main(String[] args) {
+		 String message = "Hello,\n\t\"Java\"!";
+		 // we use the escape sequence \n for a new line and \t for a tab. 
+		 
+		 System.out.println(message);
+	 }
+
+}
