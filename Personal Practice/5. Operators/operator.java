912bf0272e4f73a3ed075f92d9255066257639be
Ayush Mishra <ayushbm84@gmail.com>
2023-07-06 20:45:17 +0530

    Create operator.java

---
+++ Personal Practice/5. Operators/operator.java
@@ -0,0 +1,32 @@
+
+
+	// Operators are used to perform operations on variables and values.
+
+public class operators {
+	public static void main(String[] args) {
+		// In the example below, we use the + operator to add together two values:
+		int x = 100 + 645;
+		System.out.println(x);
+		
+		/*
+		 Arithmetic Operators [Arithmetic operators are used to perform common mathematical operations.] :
+		 + , - , * , / , % (Modulus) , ++ , -- .
+		 
+		 Assignment Operators [Assignment operators are used to assign values to variables.] :
+		 = , += , -= , *= , /= , %= , &= , |= , ^= , >>= , <<= 
+		 
+		 Comparison Operators [Comparison operators are used to compare two values (or variables).] :
+		 == , != , > , < , >= , <= 
+		 
+		 Logical Operators [Logical operators are used to determine the logic between variables or values] :
+		 && , || , ! 
+		 */
+		
+		int a = 12;
+		a++; // a = a + 1;
+		System.out.println(a);
+		
+
+	}
+ 
+}
