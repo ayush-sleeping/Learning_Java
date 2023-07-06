ac69bc66423f1dbf9dffd82b6b56fa482a9e8e83
Ayush Mishra <ayushbm84@gmail.com>
2023-07-07 00:02:37 +0530

    adding

---
+++ Personal Practice/11. Loops/Loops.java
@@ -0,0 +1,22 @@
+
+public class Loops {
+	
+	/*
+	 * Loops in Java allow you to repeat a block of code multiple times 
+	 * until a certain condition is met. 
+	 * They provide a convenient way to perform repetitive tasks 
+	 * and iterate over collections or arrays.
+	 
+	 
+	 
+	 * Loops can execute a block of code as long as a specified condition is reached.
+	 * Loops are handy because they save time, reduce errors, and they make code more readable.
+	 */
+	
+	/* Types of Loops :
+	 * For loop,
+	 * While Loop,
+	 * Do while Loop
+	 */
+
+}
