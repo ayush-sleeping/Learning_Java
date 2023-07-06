7910ae8baed5722bf05f9b62a180ca21a31cb499
Ayush Mishra <ayushbm84@gmail.com>
2023-07-06 22:01:51 +0530

    added

---
+++ Personal Practice/6.  Java Strings/NumberString.java
@@ -0,0 +1,20 @@
+
+public class NumberString {
+	
+	/* 
+	 * In Java, you can concatenate numbers with strings using the + operator.
+	 * The numbers are automatically converted to strings before concatenation.
+	 */
+	
+
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 1.75;
+
+     // Concatenating a number with a string
+        String message = "I am " + age + " years old and my height is " + height + " meters.";
+        System.out.println(message); 
+        // Output: I am 25 years old and my height is 1.75 meters.
+    }
+
+}
