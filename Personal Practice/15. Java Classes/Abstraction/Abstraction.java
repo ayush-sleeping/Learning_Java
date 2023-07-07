b96d413bae7e969fd35c7b06c3c66a3f6414a866
Ayush Mishra <ayushbm84@gmail.com>
2023-07-07 12:42:21 +0530

    added

---
+++ Personal Practice/15. Java Classes/Abstraction/Abstraction.java
@@ -0,0 +1,28 @@
+package Abstraction;
+
+public class Abstraction {
+	
+	/* Abstraction is the process of simplifying complex systems by breaking them down into smaller, 
+	        more manageable units. 
+	 * In Java, abstraction is achieved through abstract classes and interfaces. 
+	 * Abstract classes provide a blueprint for subclasses and may contain both abstract 
+	        and concrete methods. Interfaces define a contract of methods that a class must implement. 
+	 * Abstraction helps in reducing code complexity, promoting code reuse, 
+	        and providing a clear separation between interface and implementation.*/
+	
+	
+	
+	/* Data abstraction is the process of hiding certain details 
+	        and showing only essential information to the user.
+	 * Abstraction can be achieved with either abstract classes or interfaces.
+	  
+	 * The abstract keyword is a non-access modifier, used for classes and methods:
+	  
+	  
+	 * Abstract class: is a restricted class that cannot be used to create objects 
+	          (to access it, it must be inherited from another class).
+	 
+	 * Abstract method: can only be used in an abstract class, and it does not have a body. 
+	          The body is provided by the subclass (inherited from).*/
+
+}
