9028083128df9c7534ffe0ced2f34e4ea36ddf41
Ayush Mishra <ayushbm84@gmail.com>
2023-07-07 12:08:08 +0530

    added

---
+++ Personal Practice/15. Java Classes/Inheritance/Inheritance.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+
+public class Inheritance {
+	
+	/* Inheritance is a mechanism in Java that allows a class to inherit properties 
+	      and behaviors from another class.
+	 * The class that inherits is called the subclass or child class, 
+	      and the class being inherited from is called the superclass or parent class.*/
+
+
+
+  /* In Java, it is possible to inherit attributes and methods from one class to another. 
+     We group the "inheritance concept" into two categories:
+       * subclass (child) - the class that inherits from another class
+       * superclass (parent) - the class being inherited from
+     To inherit from a class, use the extends keyword.
+  */
+
+}
