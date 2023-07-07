9d27d22eb1965c3a8afd17548642ed0af7a36597
Ayush Mishra <ayushbm84@gmail.com>
2023-07-07 12:26:55 +0530

    added

---
+++ Personal Practice/15. Java Classes/Polymorphism/Polymorphism.java
@@ -0,0 +1,20 @@
+package Polymorphism;
+
+public class Polymorphism {
+	
+	/* Polymorphism means having many forms. In Java, polymorphism refers to the ability of an object 
+	              to take on multiple forms or have multiple behaviors based on the context. 
+	 * It can be achieved through method overriding and method overloading. 
+	 * Polymorphism allows for code flexibility, modularity, and dynamic binding at runtime.*/
+	
+	
+	/* Polymorphism means "many forms", and it occurs when we have many classes 
+	          that are related to each other by inheritance.
+	 * Like we specified in the previous; Inheritance lets us inherit attributes 
+	          and methods from another class. 
+	 * Polymorphism uses those methods to perform different tasks. 
+	 
+	 
+	 * This allows us to perform a single action in different ways.*/
+
+}
