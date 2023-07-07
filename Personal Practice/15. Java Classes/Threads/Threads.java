5e7b1683beea3de7272298557365117b9f8ca0fc
Ayush Mishra <ayushbm84@gmail.com>
2023-07-07 13:23:26 +0530

    Create Threads.java

---
+++ Personal Practice/15. Java Classes/Threads/Threads.java
@@ -0,0 +1,21 @@
+package Threads;
+
+public class Threads {
+	
+	/* Threads in Java can be understood as separate paths of execution within a program. 
+	 * They allow multiple tasks to run concurrently, 
+	          enhancing the program's performance and responsiveness. 
+	 * Each thread operates independently, executing its own set of instructions.	      
+	 */
+	
+	
+	/* Threads can be used to perform complicated tasks in the background 
+	      without interrupting the main program.*/
+	
+	
+	/* There are two ways to create a thread.
+	 : It can be created by extending the "Thread class" and "overriding its run() method": 
+	 
+	 */
+
+}
