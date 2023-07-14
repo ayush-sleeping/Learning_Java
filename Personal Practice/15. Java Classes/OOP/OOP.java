cbb4075c5d6fc2ff50b032b046ef9ab5757c52b8
Ayush Mishra <ayushbm84@gmail.com>
2023-07-14 20:48:37 +0530

    Create OOP.java

---
+++ Personal Practice/15. Java Classes/OOP/OOP.java
@@ -0,0 +1,51 @@
+package OOP;
+
+public class OOP {
+	
+	/* OOP in Java : 
+	   Object oriented programming is about creating objects that contain both data and methods.
+	   ( Procedural programming is about writing procedures or methods that perform operations on the data ) 
+	   
+	   
+	   It provides a way to structure and design programs based on real-world objects and their interactions. 
+	   Java is an object-oriented programming language that fully supports OOP principles. 
+	  
+	  
+	   These are some of the fundamental concepts of OOP in Java :
+	   * Classes and Objects
+	   * Encapsulation
+	   * Inheritance
+	   * Polymorphism
+	   * Abstraction
+	   * Association
+	   * Composition
+	   * Aggregation
+	   * Interfaces
+	   * Packages
+	   
+	   Understanding and applying these concepts helps in writing modular, reusable, and maintainable code. 
+	   OOP allows you to model the real world more accurately and solve complex problems efficiently.
+	   
+	   
+	   */
+	
+	/* 
+	The four pillars of Object-Oriented Programming (OOP) in Java are: :
+	 * Encapsulation
+	 * Inheritance
+	 * Polymorphism
+	 * Abstraction
+	 */
+	
+	
+	
+	/* 
+	 Object-oriented programming has several advantages over procedural programming :
+	 * OOP is faster and easier to execute
+	 * OOP provides a clear structure for the programs
+	 * OOP helps to keep the Java code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+	 * OOP makes it possible to create full reusable applications with less code and shorter development time
+	 
+	 */
+
+}
