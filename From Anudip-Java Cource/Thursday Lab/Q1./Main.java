425df415baf9be01f3e14b501e46480049e9d6d7
Ayush Mishra <ayushbm84@gmail.com>
2023-07-21 23:24:40 +0530

    updated

--- From Anudip-Java Cource/Thursday Lab/Q1./Main.java
+++ From Anudip-Java Cource/Thursday Lab/Q1./Main.java
@@ -1,1 +1,16 @@
 
+
+public class Main {
+    public static void main(String[] args) {
+    	// Creating a new instance of the Car class & assigning it to variable myCar.
+    	 Car myCar = new Car("koenigsegg", "agera rs", 2017, "Red"); 
+    	 // here "new" is indicating that a new object of the Car class will be created.
+    	 
+    	 /* In the constructor of the Car class, provided brand "koenigsegg" is passed to the super keyword, 
+    	  * which calls the constructor of the Vehicle class and sets the brand attribute to "koenigsegg"
+    	  */
+
+         myCar.drive(); // Call the drive method of the Car class
+         myCar.honk(); // Call the honk method of the Car class
+    }
+}
