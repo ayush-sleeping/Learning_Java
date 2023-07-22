b86167818b0788b8c273710784e29f5cc72c9f36
Ayush Mishra <ayushbm84@gmail.com>
2023-07-22 22:30:00 +0530

    added

---
+++ Personal Practice/15. Java Classes/Packages & API /User defined Packages Example/User_defined_Packages_Ex1.java
@@ -0,0 +1,46 @@
+package Packages_and_API;
+
+
+/* User-defined Packages (create your own packages):
+   User-defined packages are packages created by Java developers 
+      to organize their code into logical and functional units. 
+   Creating a user-defined package involves placing related classes 
+      and resources in a common directory structure. 
+   The package declaration is the first line of a Java source file 
+      and is declared using the package keyword followed by the package name.*/
+
+public class User_defined_Packages_Ex1 {
+
+	
+	/* To create a user-defined package, follow these steps:
+	 1. Create a new directory for the package and give it a meaningful name.
+
+	 2. Place the Java source files (.java files) that belong to the package in this directory.
+	 
+	 3. Add the package declaration to each source file at the beginning.
+	
+	 4. Compile the Java source files using the javac compiler, specifying the -d option 
+	        and the path to the root directory of the package.
+*/
+	
+	
+	/*
+	 Example of User-defined Package:
+	 Suppose we want to create a package named com.example and add two classes Person and Address to it. 
+	 The directory structure and code would look like this:
+
+
+      Project
+      │
+      └───src
+          │
+          └───com
+              │
+              └───example
+                      │   Person.java
+                      │   Address.java
+                
+                
+       */
+
+}
