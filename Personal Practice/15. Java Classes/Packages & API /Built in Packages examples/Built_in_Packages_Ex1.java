d43d19e8d10fd5a04b42ab830411e7dcfe59aeb0
Ayush Mishra <ayushbm84@gmail.com>
2023-07-22 22:09:03 +0530

    added

---
+++ Personal Practice/15. Java Classes/Packages & API /Built in Packages examples/Built_in_Packages_Ex1.java
@@ -0,0 +1,50 @@
+package Packages_and_API;
+
+/* The built-in packages, 
+   also known as standard or core packages, are part of the Java API. 
+   They are pre-built and provided by the Java Development Kit (JDK) 
+        and contain a wide range of classes and interfaces for various functionalities. 
+   
+   * Some examples of built-in packages include java.lang, java.util, java.io, java.net, java.awt, etc. 
+   These packages offer ready-to-use components, making it convenient for developers 
+        to perform common tasks without having to implement everything from scratch.
+*/
+
+public class Built_in_Packages_Ex1 {
+	
+// In example here I am using java.io: 
+// This package contains classes for input and output operations, including file handling.	
+
+	
+	/*
+	 import java.io.FileReader;        //  It allows us to read characters from a file.
+	 import java.io.BufferedReader;         
+	 // It allows us to read text from a character-input stream efficiently by buffering the input.
+	 import java.io.IOException;  
+	 // is a checked exception that is thrown when there is an error during input or output operations.
+
+
+
+	public class IoPackageExample {
+ 	   public static void main(String[] args) {
+   	     try (FileReader fileReader = new FileReader("example.txt");
+       	      BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+        	    String line;
+        	    while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+        	    }
+   	     } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+    	    }
+	    }
+	}
+
+
+ // This code reads the content of a file named "example.txt" using FileReader and BufferedReader, 
+ //      and then prints each line of the file to the console.
+	
+	
+*/
+
+}
