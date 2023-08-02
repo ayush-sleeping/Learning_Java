8ea4d7c1de84e26a685e8e2ffb2defe7fdcd5057
Ayush Mishra <ayushbm84@gmail.com>
2023-08-02 17:11:10 +0530

    added

---
+++ Personal Practice/15. Java Classes/Collections Framework /List/List.java
@@ -0,0 +1,50 @@
+package Collection_Framework;
+
+public class List {
+
+	public static void main(String[] args) {
+		/*
+		 List interface is part of the Java Collections Framework, which provides a set of classes 
+		 and interfaces to work with collections of objects. 
+		 The List interface is an ordered collection that allows duplicate elements 
+		 and maintains the insertion order of elements. 
+		 
+		 It is one of the most commonly used interfaces in the Collections Framework.
+		 Here are some key characteristics and features of the List interface:
+
+		Ordered Collection: Lists maintain the order in which elements are inserted. 
+		When you iterate through a List, elements are retrieved in the same order as they were added.
+
+		Duplicate Elements: Lists allow duplicate elements. 
+		You can have multiple occurrences of the same element in a List.
+
+		Indexed Access: Elements in a List can be accessed using an index. 
+		The index starts from 0 for the first element, 1 for the second element, and so on.
+
+		Methods: The List interface provides a set of methods to manipulate 
+		and access elements, such as add(), get(), remove(), indexOf(), size(), isEmpty(), and more.*/
+
+// -------------------------------------------------------------------------------------------------------------
+		
+		// Declaration of Java List Interface
+		// public interface List<E> extends Collection<E> ; 
+		
+		// In example :
+		//  List<Integer> numbers = new ArrayList<>();
+
+// -------------------------------------------------------------------------------------------------------------
+		
+		/*
+		 In the Java Collections Framework
+		  ArrayList, LinkedList, Vector  and Stack 
+		      are classes that implement the List interface. 
+		 They all represent ordered collections that allow duplicates 
+		    and maintain the insertion order of elements, as specified by the List interface. */
+
+// -------------------------------------------------------------------------------------------------------------
+				
+		 		
+
+	}
+
+}
