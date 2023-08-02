768071ccc6e5d0960073dc8f38f744c3ea4b0657
Ayush Mishra <ayushbm84@gmail.com>
2023-08-02 19:08:11 +0530

    added

---
+++ Personal Practice/15. Java Classes/Collections Framework /List/LinkedList Example/LinkedList_ex1.java
@@ -0,0 +1,36 @@
+package Collection_Framework;
+
+import java.util.LinkedList;
+
+public class LinkedList_ex1 {
+    public static void main(String[] args) {
+        // Create a LinkedList to store strings
+        LinkedList<String> names = new LinkedList<>();
+
+        // Adding elements to the LinkedList
+        names.add("Ayush");
+        names.add("Shivam");
+        names.add("Ashish");
+
+        // Accessing elements using index
+        System.out.println("First element: " + names.get(0)); 
+        // Output: First element: Alice
+
+        // Iterating through the LinkedList
+        System.out.print("LinkedList elements: ");
+        for (String name : names) {
+            System.out.print(name + " "); 
+            // Output: LinkedList elements: Alice Bob Charlie
+        }
+
+        // Removing an element from the LinkedList
+        names.remove(1); 
+        // Removes the element at index 1 (value "Bob")
+
+        // Updated size and elements in the LinkedList after removal
+        System.out.println("\nUpdated Size of the LinkedList: " + names.size()); 
+        // Output: Updated Size of the LinkedList: 2
+        System.out.println("Updated Elements in the LinkedList: " + names); 
+        // Output: Updated Elements in the LinkedList: [Alice, Charlie]
+    }
+}
